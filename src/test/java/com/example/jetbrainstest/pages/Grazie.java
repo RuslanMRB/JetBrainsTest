8de6f792f9d93110180f07ab376db71c3481ea12
RuslanMRB <ruslanqweewq123@yandex.ru>
2024-08-06 22:56:35 +0300

    Update Grazie.java

--- src/test/java/com/example/jetbrainstest/pages/Grazie.java
+++ src/test/java/com/example/jetbrainstest/pages/Grazie.java
@@ -1,11 +1,12 @@
 package com.example.jetbrainstest.pages;
 
 import com.example.jetbrainstest.AllureAttachmentsManager;
-import io.qameta.allure.Step;
+import com.example.jetbrainstest.AllureLogger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
@@ -17,7 +18,9 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Grazie  {
-    private final Logger LOG = LoggerFactory.getLogger(Grazie.class);
+    private final AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(Grazie.class));
+
+    // private final Logger LOG = LoggerFactory.getLogger(Grazie.class);
     WebDriver driver;
 
     @FindBy(css = "[aria-label='Play video']")
@@ -26,7 +29,21 @@ public class Grazie  {
     @FindBy(css = "[href='/ai/#plans-and-pricing']")
     private WebElement tafifButton;
 
-    @Step("Проверка активности кнопки запуска видео")
+    @FindBy(css = "[aria-label='Open language selection']")
+    private WebElement languageButtonAbove;
+
+    @FindBy(xpath = "//span[contains(text(),'Deutsch')]")
+    private WebElement DeutschButton;
+
+    @FindBy(css = "p.jb-offset-top-sm-24")
+    private WebElement textInPage;
+
+    @FindBy(css = "[data-test='site-logo']")
+    private WebElement mainPageButton;
+
+    @FindBy(css = "[href='/store/']")
+    private WebElement storeButton;
+
     public Boolean checkIfPlayVideoButtonIsClickable() {
         LOG.info("Проверка активности кнопки запуска видео");
         return playVideoButton.isEnabled();
@@ -36,14 +53,36 @@ public class Grazie  {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    @Step("Перешли на страницу тарифа")
+
     public void tafifButtonClick() {
-        LOG.info("Перешли на страницу тарифа");
         tafifButton.click();
+        LOG.info("Перешли на страницу тарифа");
     }
 
     public void switchToStoreTab(int num) {
         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(num - 1));
     }
+
+    public String languageButtonAboveClick() {
+        languageButtonAbove.click();
+        LOG.info("Нажали кнопку смены языка сверху");
+        DeutschButton.click();
+        LOG.info("Нажали кнопку немецкого языка");
+        return textInPage.getText();
+    }
+
+    public void mainPageButtonClick() {
+        mainPageButton.click();
+        LOG.info("Переход на главную страницу");
+    }
+
+    public void storeButtonClick() {
+        storeButton.click();
+        LOG.info("Переход в магазин");
+    }
+
+
+
+
 }
